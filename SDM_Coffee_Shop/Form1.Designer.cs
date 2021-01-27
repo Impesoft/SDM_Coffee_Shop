404eef6fca884770708fb9932320e9eb80e48167
Dries Maes <dm.inbox@outlook.com>
2021-01-27 21:24:53 +0100

    removedTWEE

--- SDM_Coffee_Shop/Form1.Designer.cs
+++ SDM_Coffee_Shop/Form1.Designer.cs
@@ -31,11 +31,9 @@ namespace SDM_Coffee_Shop
         {
             this.lblCoffee = new System.Windows.Forms.Label();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.userControl17 = new SDM_Coffee_Shop.UserControls.UserControl1();
-            this.userControl15 = new SDM_Coffee_Shop.UserControls.UserControl1();
-            this.userControl13 = new SDM_Coffee_Shop.UserControls.UserControl1();
-            this.userControl11 = new SDM_Coffee_Shop.UserControls.UserControl1();
             this.button1 = new System.Windows.Forms.Button();
+            this.userControl15 = new SDM_Coffee_Shop.UserControls.UserControl1();
+            this.userControl11 = new SDM_Coffee_Shop.UserControls.UserControl1();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -60,9 +58,7 @@ namespace SDM_Coffee_Shop
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.userControl17);
             this.splitContainer1.Panel1.Controls.Add(this.userControl15);
-            this.splitContainer1.Panel1.Controls.Add(this.userControl13);
             this.splitContainer1.Panel1.Controls.Add(this.userControl11);
             // 
             // splitContainer1.Panel2
@@ -73,13 +69,14 @@ namespace SDM_Coffee_Shop
             this.splitContainer1.SplitterWidth = 6;
             this.splitContainer1.TabIndex = 1;
             // 
-            // userControl17
+            // button1
             // 
-            this.userControl17.Location = new System.Drawing.Point(768, 606);
-            this.userControl17.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this.userControl17.Name = "userControl17";
-            this.userControl17.Size = new System.Drawing.Size(366, 551);
-            this.userControl17.TabIndex = 6;
+            this.button1.Location = new System.Drawing.Point(161, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nieuwe DriesKnop";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // userControl15
             // 
@@ -89,14 +86,6 @@ namespace SDM_Coffee_Shop
             this.userControl15.Size = new System.Drawing.Size(366, 551);
             this.userControl15.TabIndex = 4;
             // 
-            // userControl13
-            // 
-            this.userControl13.Location = new System.Drawing.Point(768, 18);
-            this.userControl13.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
-            this.userControl13.Name = "userControl13";
-            this.userControl13.Size = new System.Drawing.Size(366, 551);
-            this.userControl13.TabIndex = 2;
-            // 
             // userControl11
             // 
             this.userControl11.Location = new System.Drawing.Point(18, 18);
@@ -104,15 +93,6 @@ namespace SDM_Coffee_Shop
             this.userControl11.Name = "userControl11";
             this.userControl11.Size = new System.Drawing.Size(366, 551);
             this.userControl11.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(161, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 80);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Nieuwe DriesKnop";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -137,9 +117,7 @@ namespace SDM_Coffee_Shop
         private System.Windows.Forms.Label lblCoffee;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private UserControls.UserControl1 userControl11;
-        private UserControls.UserControl1 userControl17;
         private UserControls.UserControl1 userControl15;
-        private UserControls.UserControl1 userControl13;
         private System.Windows.Forms.Button button1;
     }
 }
