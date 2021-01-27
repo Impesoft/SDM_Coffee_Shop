8d50d0192b110c3d6f14f524021210e473a0361b
Michaël <michaeltuerlinckx@hotmail.be>
2021-01-27 13:37:21 +0100

    Deleted Button1

--- SDM_Coffee-Shop/Form1.Designer.cs
+++ SDM_Coffee-Shop/Form1.Designer.cs
@@ -29,24 +29,14 @@ namespace SDM_Coffee_Shop
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(320, 182);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "SDM Coffee Shop ";
             this.ResumeLayout(false);
@@ -54,8 +44,6 @@ namespace SDM_Coffee_Shop
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
