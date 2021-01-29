edfc0ab1e7ced96ec3f109088ac60b99a444930d
Seba Kesteloot <seba.kesteloot@gmail.com>
2021-01-29 11:15:12 +0100

    merge

--- SDM_Coffee_Shop/Forms/Form1.Designer.cs
+++ SDM_Coffee_Shop/Forms/Form1.Designer.cs
@@ -32,6 +32,9 @@ namespace SDM_Coffee_Shop
             this.lblCoffee = new System.Windows.Forms.Label();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClearCart = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -42,9 +45,10 @@ namespace SDM_Coffee_Shop
             // lblCoffee
             // 
             this.lblCoffee.AutoSize = true;
-            this.lblCoffee.Location = new System.Drawing.Point(39, 37);
+            this.lblCoffee.Location = new System.Drawing.Point(26, 24);
+            this.lblCoffee.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCoffee.Name = "lblCoffee";
-            this.lblCoffee.Size = new System.Drawing.Size(51, 20);
+            this.lblCoffee.Size = new System.Drawing.Size(35, 13);
             this.lblCoffee.TabIndex = 0;
             this.lblCoffee.Text = "label1";
             // 
@@ -52,7 +56,6 @@ namespace SDM_Coffee_Shop
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -62,9 +65,8 @@ namespace SDM_Coffee_Shop
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.button1);
-            this.splitContainer1.Size = new System.Drawing.Size(2172, 1598);
-            this.splitContainer1.SplitterDistance = 1596;
-            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.Size = new System.Drawing.Size(1409, 924);
+            this.splitContainer1.SplitterDistance = 1081;
             this.splitContainer1.TabIndex = 1;
             // 
             // flowLayoutPanel1
@@ -72,26 +74,60 @@ namespace SDM_Coffee_Shop
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1596, 1598);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1081, 924);
             this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(45, 151);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(229, 592);
+            this.flowLayoutPanel2.TabIndex = 3;
+            // 
+            // btnClearCart
+            // 
+            this.btnClearCart.Location = new System.Drawing.Point(200, 36);
+            this.btnClearCart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClearCart.Name = "btnClearCart";
+            this.btnClearCart.Size = new System.Drawing.Size(91, 34);
+            this.btnClearCart.TabIndex = 2;
+            this.btnClearCart.Text = "Maak Leeg";
+            this.btnClearCart.UseVisualStyleBackColor = true;
+            this.btnClearCart.Click += new System.EventHandler(this.btnClearCart_Click);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(132, 847);
+            this.lblPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(183, 39);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Total Price";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(160, 154);
+            this.button1.Location = new System.Drawing.Point(21, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 80);
+            this.button1.Size = new System.Drawing.Size(146, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "Nieuwe DriesKnop";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2172, 1598);
+            this.ClientSize = new System.Drawing.Size(1409, 924);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.lblCoffee);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "Form1";
             this.Text = "SDM Coffee Shop ";
             this.splitContainer1.Panel1.ResumeLayout(false);
